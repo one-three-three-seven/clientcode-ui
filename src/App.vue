af80d2289d31c24a8f121e80a5c661a1402e3438
1337 <35700046+one-three-three-seven@users.noreply.github.com>
2024-10-19 14:41:18 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <h1 class="mx-auto mt-5 text-center">Graffiti Client Codes</h1>
     <h4 class="mx-auto mt-1 mb-2 text-center">(Maintained by Sonic)</h4>
     <h4 class="mx-auto mt-1 mb-2 text-center">(Experimental)</h4>
-    <h5 class="mx-auto mt-1 mb-2 text-center">/(BU|EJ|EG|GE|NM|RH|GR|LH|LS|NB|TK|PM).*?(BU|EJ|EG|GE|NM|RH|GR|LH|LS|NB|TK|PM)/g</h5>
+    <h5 class="mx-auto mt-1 mb-2 text-center">/(BU|EJ|EG|GE|NM|RH)[0-9a-f]{0,8}(GR|LH|LS|NB|TK|PM)/g</h5>
     <a href="https://github.com/one-three-three-seven/ClientCode" class="mx-auto" target="_blank">
         <img src="@/assets/github.svg" width="30" height="30" alt="GitHub">
     </a>
