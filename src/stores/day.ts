01b828a5f947e6ef92b5dbad6a55cc30734f5d80
1337 <35700046+one-three-three-seven@users.noreply.github.com>
2024-10-19 15:15:46 +0200

    Update day.ts

--- src/stores/day.ts
+++ src/stores/day.ts
@@ -6,7 +6,7 @@ export const useDayStore = defineStore('day', () => {
     const days: Ref<Day[]> = ref([])
 
     async function fetchDays() {
-        const response = await (await fetch('https://client.codes/data')).json()
+        const response = await (await fetch('https://api.supermajority.info')).json()
         days.value = response
     }
 
