ac90eac8eaa2506f6cda248249c13d6c2e88dd66
1337 <35700046+one-three-three-seven@users.noreply.github.com>
2024-10-19 15:23:27 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <h4 class="mx-auto mt-1 mb-2 text-center">(Maintained by Sonic)</h4>
     <h4 class="mx-auto mt-1 mb-2 text-center">(Experimental)</h4>
     <h5 class="mx-auto mt-1 mb-2 text-center">/(BU|EJ|EG|GE|NM|RH|GR|LH|LS|NB|TK|PM)[0-9a-f]{0,8}(BU|EJ|EG|GE|NM|RH|GR|LH|LS|NB|TK|PM)/g</h5>
-    <a href="https://github.com/one-three-three-seven/ClientCode" class="mx-auto" target="_blank">
+    <!-- <a href="https://github.com/one-three-three-seven/ClientCode" class="mx-auto" target="_blank">
         <img src="@/assets/github.svg" width="30" height="30" alt="GitHub">
-    </a>
-    <div class="flex flex-wrap justify-center w-full h-full pt-5">
+    </a> -->
+    <div class="flex flex-wrap justify-center w-full h-full mt-5">
         <Chart />
     </div>
 </template>
